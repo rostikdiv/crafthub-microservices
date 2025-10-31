48b5671790f937c8b8a8d76dc4d9a0d692e479b0
rostikdiv <rostislavbilskij@gmail.com>
2025-10-31 15:31:51 +0200

    added notification service

---
+++ notification-service/src/main/java/com/crafthub/notification_service/KafkaListeners.java
@@ -0,0 +1,32 @@
+package com.crafthub.notification_service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j // Для логування (Lombok)
+public class KafkaListeners {
+
+    // ❗️ Головна логіка
+    // Цей метод буде АВТОМАТИЧНО викликаний,
+    // коли з'явиться нове повідомлення у топіку "orders_topic"
+    @KafkaListener(
+            topics = "orders_topic", // Назва "поштової скриньки"
+            groupId = "notification-group" // Назва "команди" (з application.yml)
+    )
+    void handleOrderNotification(String message) {
+        // У майбутньому ми б десеріалізували JSON з "message"
+        // і отримали email та деталі замовлення.
+
+        // Зараз ми просто симулюємо відправку email
+        log.info("Отримано нове сповіщення про замовлення: {}", message);
+        log.info("...симуляція відправки email...");
+
+        // Тут могла б бути логіка для @Retryable,
+        // якщо сервіс email недоступний
+    }
+
+    // Ти можеш додати більше методів @KafkaListener
+    // для інших топіків (наприклад, "password_reset_topic")
+}
